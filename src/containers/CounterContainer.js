d0a66334748fef4a33cbe3847d93b21176d689b0
ParkSeongju <parksj914@naver.com>
2020-08-31 16:44:29 +0900

    Counter-개별분리 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,10 +6,13 @@ import {increase, decrease, setDiff} from "../modules/counter";
 function CounterContainer() {
     //useSelector 는 리덕스 스토어의 상태를 조회하는 Hook
     //state 의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
-    const {number, diff} = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff
-    }));
+    //객체를 만들면 리랜더링 됨 => 개별로 분리하여 최적화
+    // const {number, diff} = useSelector(state => ({
+    //     number: state.counter.number,
+    //     diff: state.counter.diff
+    // }));
+    const number = useSelector(state => state.counter.number);
+    const diff = useSelector(state => state.counter.diff);
 
     //useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook
     const dispatch = useDispatch();
