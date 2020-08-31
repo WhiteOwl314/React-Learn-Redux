579c6c36df178b132fdcbd78279c91c9356485da
ParkSeongju <parksj914@naver.com>
2020-08-31 16:50:06 +0900

    Counter-최적화2

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useSelector, useDispatch} from "react-redux";
+import {useSelector, useDispatch, shallowEqual} from "react-redux";
 import Counter from '../components/Counter';
 import {increase, decrease, setDiff} from "../modules/counter";
 
@@ -7,12 +7,17 @@ function CounterContainer() {
     //useSelector 는 리덕스 스토어의 상태를 조회하는 Hook
     //state 의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
     //객체를 만들면 리랜더링 됨 => 개별로 분리하여 최적화
-    // const {number, diff} = useSelector(state => ({
-    //     number: state.counter.number,
-    //     diff: state.counter.diff
-    // }));
-    const number = useSelector(state => state.counter.number);
-    const diff = useSelector(state => state.counter.diff);
+    const {number, diff} = useSelector(state => ({
+        number: state.counter.number,
+        diff: state.counter.diff
+        }),
+        shallowEqual //두번째파라미터: equalityFn
+        /*
+            equalityFn? :(left: any, right: any) => boolean
+
+            shallowEqual: 객체 안의 가장겉에 있는 값들을 모두 비교
+        * */
+    );
 
     //useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook
     const dispatch = useDispatch();
